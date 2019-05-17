0076523b15bfe2be30fa56cd4af730d7056c360c
lanqier31 <gaowenqi102>
2019-05-17 16:45:41 +0800

    del

--- AutoTest/src/common/utils.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.support import expected_conditions as EC
-import time,os
-from src.common.BasePage import BasePage
-from src.common.LoginPage import LoginPage
-from src.common import log
-from selenium.webdriver.support.ui import WebDriverWait
-from config import baseconf
-
